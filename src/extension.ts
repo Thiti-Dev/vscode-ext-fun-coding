153a0f96625b2175ce84faf7e4dd146488732848
Thiti-Dev <thiti.mwk.main@gmail.com>
2022-12-27 11:32:17 +0700

    created initialization on activate

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,16 @@
 import * as vscode from "vscode";
+import { FCPlugin } from "./fun-coding/fc-plugin";
+import { SimulateShaker } from "./simulate-shaker/simulate-shaker";
 
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+  console.log("Extension loaded");
+  const shakerInstance = new SimulateShaker();
+  const fcPlugin = new FCPlugin(shakerInstance);
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument(fcPlugin.onTyping.bind(fcPlugin))
+  );
+}
 
-export function deactivate() {}
+export function deactivate() {
+  console.log("Extension unloaded");
+}
